64850827c975328ae55326224c21d05654f4125e
Ernesto Diaz <diaz37@usf.edu>
2025-10-05 23:57:15 -0400

    step 1 complete

--- cc_6.js
+++ cc_6.js
@@ -1,6 +1,13 @@
 // create employee class
 class Employee {
     constructor(name, department) {
-
+        this.name = name;
+        this.department = department;
     }
+
+    // describe method to return formatted string
+    describe() {
+        return `${this.name} works in the ${this.department} department.`;
+    }
+
 }
