06bafd66bed5e9d2af2817c7851c0eabf0a72c4b
Ernesto Diaz <diaz37@usf.edu>
2025-10-06 00:01:13 -0400

    step 4 complete

--- cc_6.js
+++ cc_6.js
@@ -11,3 +11,22 @@ class Employee {
     }
 
 }
+
+// create manager class with extension
+class Manager extends Employee {
+    constructor(name, department, teamSize) {
+        super(name, department);
+        this.teamSize = teamSize;
+    }
+
+    // describe method to override parent method
+    describe() {
+        return `${this.name} manages a team of ${this.teamSize} people in the ${this.department} department.`;
+    }
+}
+
+// create sample employees and managers
+const emp1 = new Employee("Jake","IT");
+const emp2 = new Employee("Amy","HR");
+const mng1 = new Manager("Tom","IT",5);
+const mng2 = new Manager("Sara","HR",3);
